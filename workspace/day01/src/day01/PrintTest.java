5c8b2ea4cebf102e1fd1178457b44cd3ce245e59
DV-DaVinci <103577251+ElenaSyndrome@users.noreply.github.com>
2022-04-14 20:19:41 +0900

    PrintTest

---
+++ workspace/day01/src/day01/PrintTest.java
@@ -0,0 +1,19 @@
+package day01;
+
+//Ctrl + Shift + : 글자크기 크게
+//Ctrl + Shift - : 글자크기 작게
+public class PrintTest {
+	// 한줄주석
+	/*
+	 * 여러줄 주석
+	 */
+
+	// main 메소드 : JVM이 실행시켜주는 메소드
+	// 프로그램의 시작 == 진입점
+	//Ctrl+Shift+F : 자동정렬
+	//Ctrl+spacebar : 자동완성
+	//Ctrl+F11 : 실행
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
---
+++ workspace/day01/src/day01/PrintTest2.java
@@ -0,0 +1,57 @@
+package day01;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+//		Ctrl+/ : 한줄주석
+//		System.out.printf("%d",10);
+//		System.out.printf("안녕하세요 저는 %d살입니다.",10);
+//		%.3f : 소수점 3자리까지 출력
+//		System.out.printf("%d번째 실수 : %.3f", 1, 2.28);
+		
+		//"
+//		System.out.printf("\"");
+		
+//		System.out.printf("이름 : %s\n", "Elena");
+//		System.out.printf("일하는곳 : %s\n", "JAVA");
+		
+		//이름 : 홍길동
+		//나이 : 10살
+		//키 : 180.27cm
+		//몸무게 : 75.24kg
+		//점수 : A
+		
+//		Ctrl+Alt+↑(↓) : ↑(↓)로 한줄 복사
+//		Alt+↑(↓) : ↑(↓)로 이동
+/*		"안녕하세요"
+		System.out.printf("\"안녕하세요\"\n");
+//		이름: Elena
+		System.out.printf("이름: %s\n", "Elena");
+//		나이: 27살
+		System.out.printf("나이: %d살\n", 27);
+//		키: 150.27cm
+		System.out.printf("키: %.2fcm\n", 150.27);
+//		몸무게: 45.24kg
+		System.out.printf("몸무게: %.2fkg\n", 45.24);
+//		점수: A
+		System.out.printf("점수: %c\n",'A');
+//			\o/
+		System.out.printf("\t\\o/");
+*/		
+		
+//		System.out.print("나이 : ");
+//		System.out.print(10);
+//		System.out.print("살\n");
+		
+		//문자열 + 값 : 하나의 문자열로 연결 ("홍"+"길동" --> "홍길동")
+//		System.out.print("나이:"+10+"살\n");
+		
+		//syso + 자동완성
+		System.out.println("나이:"+10+"살\n");
+		//1+1=2
+		System.out.println(1+"+"+1+"="+(1+1));
+		System.out.printf("%d + %d = %d", 1,1,1+1);
+		
+		
+		
+	}
+}
